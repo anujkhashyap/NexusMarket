41e21ad6b49ce9173f460800fd1cbc307667128d
Anuj <anuj6353@gmail.com>
2026-02-10 01:12:08 +0530

    NexusMarket backend setup intial commit

---
+++ NexusMarket/src/test/java/com/example/NexusMarket/NexusMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NexusMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
